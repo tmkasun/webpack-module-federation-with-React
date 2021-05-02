5f6c38dc27ee50c0a4d069e11256320865e67e53
tmkasun <tmkasun@gmail.com>
2021-05-02 23:05:50 +0530

    update URL

--- app1/webpack.config.js
+++ app1/webpack.config.js
@@ -53,7 +53,7 @@ const config = {
     new ModuleFederationPlugin({
       name: 'application_master',
       remotes: {
-        application_b: 'application_b@',
+        application_b: 'application_b@https://knnect-mf-app2.netlify.app/bRemoteEntry.js',
       },
       shared: { react: { eager: true }, 'react-dom': { eager: true } },
     }),
